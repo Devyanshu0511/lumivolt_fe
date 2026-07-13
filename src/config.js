d37594d7497b4504bc9d8e4492e3902c0b85ecfc
akulvarshney <akulv.work@gmail.com>
2026-07-14 00:06:21 +0530

    update API_BASE_URL to point to production environment default

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const env = "PROD";
+export const API_BASE_URL = env === "PROD" ? 'https://www.lumivolt.co.in' : 'http://localhost:3001';
